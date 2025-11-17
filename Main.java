ec7c2dc74f4e64b206962a27bc768fb078f5448c
Pecel Antartika <hamamyusufa@gmail.com>
2025-11-17 20:56:11 +0700

    Remove comments from Main.java
    Removed comments to clean up the code.

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Membuat contoh produk
         Produk p1 = new Produk("Teh Botol", "M001", 5000, 20, "Minuman");
         Produk p2 = new Produk("Roti", "M002", 8000, 10, "Makanan");
 
@@ -19,22 +18,18 @@ public class Main {
 
         Produk barangDipilih = (pilih == 1) ? p1 : p2;
 
-        // Input jumlah
         System.out.print("Masukkan jumlah: ");
         int jumlah = input.nextInt();
 
-        // Masukkan ke keranjang
         keranjang.simpanProduk(barangDipilih, jumlah);
         barangDipilih.hapusStock(jumlah);
 
         System.out.println("\nBarang berhasil dimasukkan ke keranjang!");
         System.out.println("Total sementara = Rp" + keranjang.getHarga());
 
-        // Cek member
         System.out.print("\nApakah Anda member? (true/false): ");
         boolean isMember = input.nextBoolean();
 
-        // Checkout
         Checkout co = new Checkout(isMember, keranjang.getHarga(), "TRX001");
 
         System.out.println("\n=== MENCETAK STRUK ===");
